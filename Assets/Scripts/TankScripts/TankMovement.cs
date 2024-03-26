2ece5507217bfa521fc64865f5f8f93fadb7f9f3
Batuhan Yildiz <yildiz.batuhan017@gmail.com>
2024-03-26 11:22:41 +0300

    TankMovement Updated

--- Assets/Scripts/TankScripts/TankMovement.cs
+++ Assets/Scripts/TankScripts/TankMovement.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class TankMovement : MonoBehaviour
 {
+    public float moveSpeed = 5f; // Tankın hareket hızı
+    public float rotationSpeed = 100f;
+    public TankScripts.Inputs inputs; // Inputs sınıfına erişmek için referans
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,19 @@ public class TankMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Vector2 inputDirection = inputs.joystickVec; // Joystick giriş değerini al
+
+        // Joystick vektörünü dünya koordinatlarına göre dönüştür
+        Vector3 moveDirection = new Vector3(inputDirection.x, 0f, inputDirection.y).normalized;
+
+        // Hareketi uygula
+        transform.Translate(moveDirection * moveSpeed * Time.deltaTime, Space.World);
+        // Tankın dönme işlemini gerçekleştir
+        if (moveDirection != Vector3.zero)
+        {
+            Quaternion targetRotation = Quaternion.LookRotation(moveDirection); // Hedef rotasyonu belirle
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime); // Dönme işlemini uygula
+        }
     }
+    
 }
